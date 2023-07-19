1a9053fa936e4635a643ad69f5cc20cafbfa458b
Paris Charalampidis <curlycode@outlook.com>
2023-07-19 09:10:03 +0300

    added footer content and image folder

--- web_development/websites/langshare/partials/footer.php
+++ web_development/websites/langshare/partials/footer.php
@@ -1,6 +1,55 @@
 </div>
 
-</body>
+
+<div class="flex justify-center items-center bg-red-700  w-full h-1/3">
+    <div class="container">
+
+        <div class="flex justify-center items-center text-gray-300 ">
+            <div class="mt-6">
+                <div class="flex flex-row">
+                    <div class="flex flex-col">
+                        <div class="font-bold text-3xl">
+                            <h1>Langshare</h1>
+                        </div>
+                        <div class="mt-5">
+                            <p>Copyright &copy; 2023</p>
+                            <p>All rights reserved</p>
+                        </div>
+                    </div>
+                    <div class="flex flex-col mx-5">
+                        <div class="font-bold text-3xl">
+                            <h1>EΠΙΚΟΙΝΩΝΙΑ</h1>
+                        </div>
+                        <div class="flex mt-4">
+                            <h3 class="font-bold">email:</h3>
+                            <p class="mx-3">info@langshare.gr</p>
+                        </div>
+                        <div class="flex mt-1">
+                            <h3 class="font-bold">website:</h3>
+                            <p class="mx-3">www.langshare.gr</p>
+                        </div>
+                    </div>
+
+                    <div class="flex flex-col">
+                        <div class="font-bold text-3xl">
+                            <h1>MENU</h1>
+                        </div>
+                        <div class="flex flex-col font-bold mt-4">
+                            <h3>ΑΡΧΙΚΗ</h3>
+                            <h3>ΔΗΜΙΟΥΡΓΙΑ ΕΓΓΡΑΦΗΣ</h3>
+                            <h3>ΕΠΙΣΚΟΠΗΣΗ</h3>
+                            <h3>ΣΧΕΤΙΚΑ</h3>
+                            <h3>ΕΠΙΚΟΙΝΩΝΙΑ</h3>
+                        </div>
+
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+    </body>
 
 
-</html>
+    </html>
